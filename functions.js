1c6c83a429457489711bc78866869089831ee260
Lander Sola <landersolaaguirre@gmail.com>
2023-11-20 13:33:23 +0100

    corrections

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ export const generarCombinacionAleatoria = () => {
     var caracteres = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     var combinacion = ''
 
-    for (var i = 0 i < 7 i++) {
+    for (var i = 0; i < 7; i++) {
         var indiceAleatorio = Math.floor(Math.random() * caracteres.length)
         combinacion += caracteres.charAt(indiceAleatorio)
     }
